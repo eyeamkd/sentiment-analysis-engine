31395aa9e6ea46434d90b23b783cedd7b995cda2
Kunal Dubey <kunaldubey2297@gmail.com>
2021-09-29 14:47:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict"; 
+require('dotenv').config();
+const key = process.env.KEY;
+const endpoint = process.env.ENDPOINT;
+
+const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics"); 
+const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key)); 
+
+async function sentimentAnalysis(client){
+
+    const sentimentInput = [
+        "I had the best day of my life. I wish you were there with me."
+    ];
+    const sentimentResult = await client.analyzeSentiment(sentimentInput);
+
+    sentimentResult.forEach(document => {
+        console.log(`ID: ${document.id}`);
+        console.log(`\tDocument Sentiment: ${document.sentiment}`);
+        console.log(`\tDocument Scores:`);
+        console.log(`\t\tPositive: ${document.confidenceScores.positive.toFixed(2)} \tNegative: ${document.confidenceScores.negative.toFixed(2)} \tNeutral: ${document.confidenceScores.neutral.toFixed(2)}`);
+        console.log(`\tSentences Sentiment(${document.sentences.length}):`);
+        document.sentences.forEach(sentence => {
+            console.log(`\t\tSentence sentiment: ${sentence.sentiment}`)
+            console.log(`\t\tSentences Scores:`);
+            console.log(`\t\tPositive: ${sentence.confidenceScores.positive.toFixed(2)} \tNegative: ${sentence.confidenceScores.negative.toFixed(2)} \tNeutral: ${sentence.confidenceScores.neutral.toFixed(2)}`);
+        });
+    });
+}
+
+sentimentAnalysis(textAnalyticsClient);
