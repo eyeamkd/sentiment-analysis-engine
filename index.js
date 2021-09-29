dfd42992fb6348a53a61c9885ecae9c701784dba
Kunal Dubey <kunaldubey2297@gmail.com>
2021-09-29 15:09:32 +0530

    converted the function into an express server

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-"use strict"; 
-require('dotenv').config();
-const key = process.env.KEY;
-const endpoint = process.env.ENDPOINT;
+"use strict";
+const sentimentAnalysis = require('./engine');
+var bodyParser = require('body-parser');
+const express = require('express');
 
-const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics"); 
-const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key)); 
+var app = express()
 
-async function sentimentAnalysis(client){
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
 
-    const sentimentInput = [
-        "I had the best day of my life. I wish you were there with me."
-    ];
-    const sentimentResult = await client.analyzeSentiment(sentimentInput);
+// parse application/json
+app.use(express.json())
 
-    sentimentResult.forEach(document => {
-        console.log(`ID: ${document.id}`);
-        console.log(`\tDocument Sentiment: ${document.sentiment}`);
-        console.log(`\tDocument Scores:`);
-        console.log(`\t\tPositive: ${document.confidenceScores.positive.toFixed(2)} \tNegative: ${document.confidenceScores.negative.toFixed(2)} \tNeutral: ${document.confidenceScores.neutral.toFixed(2)}`);
-        console.log(`\tSentences Sentiment(${document.sentences.length}):`);
-        document.sentences.forEach(sentence => {
-            console.log(`\t\tSentence sentiment: ${sentence.sentiment}`)
-            console.log(`\t\tSentences Scores:`);
-            console.log(`\t\tPositive: ${sentence.confidenceScores.positive.toFixed(2)} \tNegative: ${sentence.confidenceScores.negative.toFixed(2)} \tNeutral: ${sentence.confidenceScores.neutral.toFixed(2)}`);
-        });
-    });
-}
+const port = 3000
 
-sentimentAnalysis(textAnalyticsClient);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/analyse',async (req,res)=>{ 
+    try{
+        console.log("Text being passed is",req.body.text);
+        const sentiment = await sentimentAnalysis(req.body.text); 
+        res.status(200).send(sentiment[0]); 
+        //res.send("Ok").status(200);
+    } 
+    catch(e){
+        return res.status(400).send("Error Occured",e);
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
